514cb40620fa93c6327ecc0fc0f5b7057687283b
Isaiah Frantz <Isaiah.Frantz1@T-Mobile.com>
2023-08-03 17:08:02 -0700

    tests args, moved things around

--- ugly.py
+++ ugly.py
@@ -12,10 +12,11 @@ Ugly monitoring app
 # stdlib imports
 import getopt
 import sys
+import os
 import re
 
-# ugly module
-from ugly_lib import Ugly
+# ugly client classes
+from ugly_lib import *
 
 # remove first arg/program name
 argslist = sys.argv[1:]
@@ -36,107 +37,6 @@ long_options = ["help",
                 "nfs_write_dir=",
                 ]
 
-try:
-    # Parse args
-    args, values = getopt.getopt(argslist, OPTIONS, long_options)
-except getopt.error as err:
-        # we hit an error, print details
-        pm(f"ERROR: arg parsing error=>{str(err)}<")
-
-# globals and default values
-# ARCH = 'ARCH_NOT_SET'
-# NUM  = 10
-
-def main(args) -> int:
-    # default values:
-    input_type = 'nfs'
-    input_target = 'path-to-ip-lists.txt'
-    scan_type = 'agent-pull'
-    max_agent_pull_retries = 10
-    nfs_read_dir = '/nfs/agent-output'
-    storage_type = 's3'
-    s3_region = 'eu-west-1'
-    s3_bucket_prefix = 'ip-scanner-results'
-    nfs_write_dir = '/nfs/ip-scanner-results'
-
-    # check args
-    for arg, val in args:
-        if arg in ("-h", "--help"):
-            pm("Display help")
-            show_help()
-
-        elif arg in ("-d", "--debug"):
-            pm("Enable debug output")
-            u.DEBUG = True
-
-        elif arg in ("-i", "--input_type"):
-            input_type = val
-            # TODO add validation code:
-            # can be 'nfs' or 'api'
-
-        elif arg in ("-g", "--input_target"):
-            input_target = val
-            # TODO add validation code:
-            # if input_type == nfs, should be a file that exists
-            # if input_type == api, should be valid uri (test existence later in )
-
-        elif arg in ("-s", "--scan_type"):
-            scan_type = val
-            # TODO add validation code
-            # can be 'agent-pull' or 'nfs-read'
-
-        elif arg in ("-m", "--max_agent_pull_retries"):
-            max_agent_pull_retries = val
-            # TODO add validation code
-
-        elif arg in ("-n", "--nfs_read_dir"):
-            nfs_read_dir = val
-            # TODO add validation code
-
-        elif arg in ("-t", "--storage_type"):
-            storage_type = val
-            # TODO add validation code
-            # can be 's3' or 'nfs-write'
-
-        elif arg in ("-r", "--s3_region"):
-            s3_region = val
-            # TODO add validation code
-
-        elif arg in ("-p", "--s3_bucket_prefix"):
-            s3_bucket_prefix = val
-            # TODO add validation code
-
-        elif arg in ("-o", "--nfs_write_dir"):
-            nfs_write_dir = val
-            # TODO add validation code
-
-        # populate ip_list array
-        ip_list = None
-        if input_type == 'nfs':
-            ip_list = u.open_target_file(input_target)
-        elif input_type == 'api':
-            ip_list = u.open_api(input_target)
-        if ip_list is None:
-            raise Exception(f"unrecognized input_type {input_type}")
-
-        results = dict()
-        if scan_type == 'agent-pull':
-            results = u.agent_pull(ip_list, max_agent_pull_retries)
-        elif scan_type == 'nfs-read':
-            results = u.nfs_read(ip_list, nfs_read_dir)
-        else:
-            raise Exception(f"unrecognized scan_type {scan_type}")
-
-        if storage_type == 's3':
-            u.storeResultsInS3(results, s3_region, s3_bucket_prefix)
-        elif storage_type == 'nfs-write':
-            u.nfs_write(results, nfs_write_dir)
-        else:
-            raise Exception(f"unrecognized storage_type {storage_type}")
-
-
-        return 0
-
 def show_help(exit_code=0):
     '''
     Display help and exit
@@ -145,7 +45,8 @@ def show_help(exit_code=0):
         Returns:
             None
     '''
-    print ("""Args:
+    print ("""show_help()
+    Args:
     -h,--help                       display this help
     -d,--debug                      print debug output
     -i,--input_type                 where input data is coming from
@@ -175,6 +76,115 @@ def pm(message):
     else:
         print(message)
 
+try:
+    # Parse args
+    args, values = getopt.getopt(argslist, OPTIONS, long_options)
+except getopt.error as err:
+        # we hit an error, print details
+        pm(f"ERROR: arg parsing error=>{str(err)}<")
+
+# globals and default values
+DEBUG = False
+
+def main(args) -> int:
+    # get values from env or use defaults before checking for optional cli options
+    input_type = os.environ('INPUT_TYPE') if os.environ('INPUT_TYPE') else 'nfs'
+    input_target = os.environ('INPUT_TARGET') if os.environ('INPUT_TARGET') else 'path-to-ip-lists.txt'
+    # scan_type = 'agent-pull'
+    max_agent_pull_retries = os.environ('MAX_AGENT_PULL_RETRIES') if os.environ('MAX_AGENT_PULL_RETRIES') else 10
+    nfs_read_dir = os.environ('NFS_READ_DIR') if os.environ('NFS_READ_DIR') else '/nfs/agent-output'
+    # storage_type = 's3'
+    s3_region = os.environ('S3_REGION') if os.environ('S3_REGION') else 'eu-west-1'
+    s3_bucket_prefix = os.environ('S3_BUCKET_PREFIX') if os.environ('S3_BUCKET_PREFIX') else 'ip-scanner-results'
+    nfs_write_dir = os.environ('NFS_WRITE_DIR') if os.environ('NFS_WRITE_DIR') else '/nfs/ip-scanner-results'
+    debug = os.environ('DEBUG') if os.environ('DEBUG') else False
+
+    # check args
+    for arg, val in args:
+        if arg in ("-h", "--help"):
+            pm("Display help")
+            show_help()
+
+        elif arg in ("-d", "--debug"):
+            pm("Enable debug output")
+            DEBUG = True
+
+        elif arg in ("-i", "--input_type"):
+            input_type = val
+            # TODO add validation code:
+            # can be 'nfs' or 'api'
+
+        elif arg in ("-g", "--input_target"):
+            input_target = val
+            # TODO add validation code:
+            # if input_type == nfs, should be a file that exists
+            # if input_type == api, should be valid uri (test existence later in )
+
+        # elif arg in ("-s", "--scan_type"):
+        #     scan_type = val
+        #     # TODO add validation code
+        #     # can be 'agent-pull' or 'nfs-read'
+
+        elif arg in ("-m", "--max_agent_pull_retries"):
+            max_agent_pull_retries = val
+            # TODO add validation code
+
+        elif arg in ("-n", "--nfs_read_dir"):
+            nfs_read_dir = val
+            # TODO add validation code
+
+        # elif arg in ("-t", "--storage_type"):
+        #     storage_type = val
+        #     # TODO add validation code
+        #     # can be 's3' or 'nfs-write'
+
+        elif arg in ("-r", "--s3_region"):
+            s3_region = val
+            # TODO add validation code
+
+        elif arg in ("-p", "--s3_bucket_prefix"):
+            s3_bucket_prefix = val
+            # TODO add validation code
+
+        elif arg in ("-o", "--nfs_write_dir"):
+            nfs_write_dir = val
+            # TODO add validation code
+
+        # required arg groups
+        required_args = {'input_type', 'input_target'}
+        required_arg_by_type = {
+                'nfs': {'nfs_read_dir', 'nfs_write_dir'},
+                'api': {'max_agent_pull_retries', 's3_region', 's3_bucket_prefix'}
+            }
+
+        # Check required input_type args have been defined
+        if input_type not in required_arg_by_type.keys():
+            raise getopt.GetoptError(f'input_type must be one of=>{Ugly.GetInputTypes}<, got=>{input_type}<')
+        for v in required_args + required_arg_by_type[input_type]:
+            if v not in locals():
+                raise getopt.GetoptError(f'Required arg for input_type=>{input_type}< missing=>{v}<')
+
+        # Warn if args have been defined for the wrong input type
+        not_required_args = set(required_arg_by_type.keys()) - set(input_type)
+        for k,vars in required_arg_by_type:
+            if k == input_type: continue
+            for v in vars:
+                if v in locals():
+                    raise Warning(f'Found arg=>{v}< that is required for input_type=>{k}< but unused for selected input_type=>{input_type}<')
+
+        # create a storage type specific client
+        if input_type == 'nfs':
+            client = Nfs_client(input_target, nfs_read_dir, nfs_write_dir, DEBUG)
+        elif input_type == 'api':
+            client = S3_client(input_target, max_agent_pull_retries, s3_region, s3_bucket_prefix, debug)
+        else:
+            raise Exception(f"unrecognized input_type {input_type}")
+
+        # process the data and write to output storage
+        client.process()
+
+        # if we made it this far without an exception, exit 0
+        return 0
 
 # start main script
 if __name__ == '__main__':
